84ce3e0646b33da9d893110190624118bcaf83ae
mrospond <marekrospond@gmail.com>
2022-09-05 00:38:20 +0200

    image upload engine

--- app.py
+++ app.py
@@ -1,14 +1,53 @@
-from flask import Flask, render_template, jsonify, redirect
+from flask import Flask, render_template, jsonify, redirect, send_from_directory, url_for
+
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
+
 from test.second import second
 
 app = Flask(__name__)
 app.register_blueprint(second, url_prefix="/admin")
 
+app.config["SECRET_KEY"] = "asdfghjkl"
+app.config["UPLOADED_PHOTOS_DEST"] = "uploads"
+
+photos = UploadSet("photos", IMAGES)
+configure_uploads(app, photos)
+
+class UploadForm(FlaskForm):
+    photo = FileField(
+        validators=[
+            FileAllowed(photos, "Only images are allowed"),
+            FileRequired("File field should not be empty")
+        ]
+    )
+    submit = SubmitField("Upload")
+
+@app.route("/uploads/<filename>")
+def get_file(filename):
+    return send_from_directory(app.config["UPLOADED_PHOTOS_DEST"], filename)
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def upload_image():
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        file_url = url_for("get_file", filename=filename)
+    else:
+        file_url = None
+
+    return render_template("index.html", form=form, file_url=file_url)
+
+
 # @app.route('/home')
-@app.route("/")
-def hello_world():
-    title = "memeGen"
-    return render_template("home.html", title=title)
+# @app.route("/")
+# def hello_world():
+#     title = "memeGen"
+#     return render_template("home.html", title=title)
 
 
 
